2f798d3b972b60d5b48f3faa4e3e060269e378b1
A-Startled-Bystander <jastuartross@gmail.com>
2025-07-20 22:38:57 +0200

    Extract all text from a pdf doc and break it up into its pages

---
+++ Utils/DocumentUtils.py
@@ -0,0 +1,33 @@
+import re
+import pymupdf
+
+from pdfminer.high_level import extract_text_to_fp
+from pdfminer.layout import LAParams
+from io import StringIO
+
+def convert_pdf_to_text(path: str):
+    output = StringIO()
+    with open(path, "rb") as f:
+        extract_text_to_fp(
+            f,
+            output,
+            laparams=LAParams(),
+            output_type='text',
+            codec=None
+        )
+
+    text = output.getvalue()
+    output.close()
+    del output
+    return text
+
+def split_page_breaks(pdf_text: str):
+    pages = pdf_text.split("\x0c") #Split at every page break
+    return_pages = []
+    for i, page in enumerate(pages, 1):
+        if page.strip() != '' and page.strip() != '\n':
+            return_pages.append({
+                "page_num": i,
+                "page_content": page
+            })
+    return return_pages
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Init Commit')
+import os
+
+from Utils.DocumentUtils import convert_pdf_to_text, split_page_breaks
+
+currentDirectory = os.path.dirname(os.path.abspath(__file__))
+exampleDoc = "Soilless Culture - Use of Substrates for the Production of Quality Horticultural Crops (Md. Asaduzzaman) (Z-Library).pdf"
+PDF_PATH = currentDirectory + "\\Data\\Documents\\" + exampleDoc
+
+
+# text = extract_text_from_doc(PDF_PATH)
+text = convert_pdf_to_text(PDF_PATH)
+pages = split_page_breaks(text)
+print()
