73a3660c0cae27ca8d929eab5b010b9c5bb0e0c8
A-Startled-Bystander <jastuartross@gmail.com>
2025-07-21 17:49:07 +0200

    id table of contents

--- Utils/DocumentUtils.py
+++ Utils/DocumentUtils.py
@@ -6,6 +6,7 @@ from pdfminer.layout import LAParams
 from io import StringIO
 
 def convert_pdf_to_text(path: str):
+    print("begin extraction of pdf text")
     output = StringIO()
     with open(path, "rb") as f:
         extract_text_to_fp(
@@ -19,6 +20,7 @@ def convert_pdf_to_text(path: str):
     text = output.getvalue()
     output.close()
     del output
+    print("Text extraction completed")
     return text
 
 def split_page_breaks(pdf_text: str):
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from Utils.DocumentUtils import convert_pdf_to_text, split_page_breaks
+from Utils.LLMUtils import identify_table_of_contents
 
 currentDirectory = os.path.dirname(os.path.abspath(__file__))
 exampleDoc = "Soilless Culture - Use of Substrates for the Production of Quality Horticultural Crops (Md. Asaduzzaman) (Z-Library).pdf"
@@ -10,4 +11,6 @@ PDF_PATH = currentDirectory + "\\Data\\Documents\\" + exampleDoc
 # text = extract_text_from_doc(PDF_PATH)
 text = convert_pdf_to_text(PDF_PATH)
 pages = split_page_breaks(text)
+identify_table_of_contents(pages[:10])
 print()
+
