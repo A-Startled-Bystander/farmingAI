72399d9fde8e394a1c9adb98e9e1e0d84906e5a7
A-Startled-Bystander <jastuartross@gmail.com>
2025-08-03 01:35:51 +0200

    Added file validation functionality

--- Utils/DocumentUtils.py
+++ Utils/DocumentUtils.py
@@ -1,7 +1,81 @@
+import os
 import time
 
 import fitz # PyMuPDF
+from dotenv import load_dotenv
 from pymupdf import Document
+
+def retrieve_doc_name(path: str):
+    return path.split("\\")[-1]
+
+def verify_file_validity(path: str):
+    filename = retrieve_doc_name(path)
+    try:
+        doc: Document = fitz.open(path)
+        if doc.page_count == 0:
+            return {
+                "FILE_NAME": filename,
+                "VALIDITY": False,
+                "REASON": "No Pages Found"
+            }
+        for page_number in range(len(doc)):
+            try:
+                page = doc.load_page(page_number)
+            except Exception as e:
+                return {
+                    "FILE_NAME": filename,
+                    "VALIDITY": False,
+                    "REASON": f"Could Not Load Page {page_number}"
+                }
+
+    except Exception as e:
+        return {
+            "FILE_NAME": filename,
+            "VALIDITY": False,
+            "REASON": "Could Not Open"
+        }
+
+    try:
+        load_dotenv()
+        save_path = os.getenv("VALIDATED_DOC_FILE_LOCATION") + filename
+        temp_path = save_path + ".tmp"
+
+        doc.save(temp_path)
+        os.replace(temp_path, save_path)
+
+    except Exception as e:
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
+
+        return {
+            "FILE_NAME": filename,
+            "VALIDITY": False,
+            "REASON": "Could Not Save"
+        }
+
+
+    return {
+        "FILE_NAME": filename,
+        "VALIDITY": True,
+        "REASON": "All pages Loaded"
+    }
+
+def retrieve_pages(path: str):
+    filename = retrieve_doc_name(path)
+    pages = []
+    doc: Document = fitz.open(path)
+    for page_number in range(len(doc)):
+        page = doc.load_page(page_number)
+        pages.append(page)
+
+    # search = page.search_for("Contents")
+    # if len(search) > 0:
+    #     print()
+
+    return {
+        "FILE_NAME": filename,
+        "PAGES": pages
+    }
 
 
 def convert_pdf_to_text(path: str):
@@ -23,11 +97,11 @@ def convert_pdf_to_text(path: str):
     print("Text extraction completed")
     return pages
 
-def identify_table_of_contents(pages):
-    for page in pages:
-        if "Contents" in page["page_content"]:
-
-            break
-
-    # If we cant find a page specifying "Contents" we will need to check with the LLM if any pages ID as a TOC
-    return None
+# def identify_table_of_contents(pages):
+#     for page in pages:
+#         if "Contents" in page["page_content"]:
+#
+#             break
+#
+#     # If we cant find a page specifying "Contents" we will need to check with the LLM if any pages ID as a TOC
+#     return None
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+import json
 import os
 
-from Utils.DocumentUtils import convert_pdf_to_text
+from dotenv import load_dotenv
+
+from Utils.DocumentUtils import convert_pdf_to_text, retrieve_pages, verify_file_validity
 
 currentDirectory = os.path.dirname(os.path.abspath(__file__))
 exampleDoc = "Soilless Culture - Use of Substrates for the Production of Quality Horticultural Crops (Md. Asaduzzaman) (Z-Library).pdf"
@@ -8,7 +12,37 @@ exampleDoc = "Soilless Culture - Use of Substrates for the Production of Quality
 PDF_PATH = currentDirectory + "\\Data\\Documents\\" + exampleDoc
 
 
-# text = extract_text_from_doc(PDF_PATH)
-pages = convert_pdf_to_text(PDF_PATH)
-print()
 
+# pages = convert_pdf_to_text(PDF_PATH)
+# file_validity = verify_file_validity(PDF_PATH)
+#
+
+
+
+def validate_file_sources():
+    load_dotenv()
+    unvalidated_files_path = os.getenv("UNVALIDATED_DOC_FILE_LOCATION")
+    sub_dir = os.listdir(unvalidated_files_path)
+    print()
+
+    log_name = f"farm_docs_failed_validity_{datetime.now().strftime("%Y%m%d_%H%M%S")}.txt"
+    open(log_name, "x")
+    with open(log_name, "w") as log:
+
+        for folder in sub_dir:
+            log.write("\n")
+            sub_dir_path = unvalidated_files_path + folder + "\\"
+
+            files = os.listdir(sub_dir_path)
+            log.write(f"Parent DIRECTORY: {sub_dir_path}" + "\n")
+            log.write("--------------------------------------------------" + "\n")
+            for file in files:
+                file_path = sub_dir_path + file
+                file_validity = verify_file_validity(file_path)
+                if not file_validity["VALIDITY"]:
+                    json_line = json.dumps(file_validity)
+                    log.write(json_line + "\n")
+
+
+validate_file_sources()
+print("File Scan Complete")
